8878f050af7826a86dbb71f0e091f6713cd4eecb
Daniel Kraus <daniel@kraus.my>
2017-07-23 17:15:11 +0800

    browse-at-remote; js2-refactor

--- personal/personal.el
+++ personal/personal.el
@@ -1352,7 +1352,10 @@ and when called with 2 prefix arguments copy url and open in browser."
             (browse-at-remote-kill)
             (browse-at-remote))
         (browse-at-remote))))
-  :bind (:map prelude-mode-map ("C-c G" . dakra-browse-at-remote)))
+  :bind (:map prelude-mode-map ("C-c G" . dakra-browse-at-remote))
+  :config
+  (add-to-list 'browse-at-remote-remote-type-domains '("gitlab.bis" . "gitlab"))
+  (setq browse-at-remote-prefer-symbolic nil))
 
 ;; FIXME: find another gh lib. only works for public repos and unmaintained
 ;; just type 'fixes #' and get github issue autocompletion
@@ -1574,6 +1577,51 @@ and when called with 2 prefix arguments copy url and open in browser."
   :commands indium-interaction-mode
   :init (add-hook 'js-mode-hook #'indium-interaction-mode)
   :config (setq indium-update-script-on-save t))
+
+(use-package js2-refactor
+  :commands js2-refactor-mode
+  :init (add-hook 'js2-mode-hook #'js2-refactor-mode)
+  :config
+  (define-key js2-refactor-mode-map (kbd "C-c r")
+    (defhydra js2-refactor-hydra (:color blue :hint nil)
+      "
+^Functions^                    ^Variables^               ^Buffer^                      ^sexp^               ^Debugging^
+------------------------------------------------------------------------------------------------------------------------------
+[_lp_] Localize Parameter      [_ev_] Extract variable   [_wi_] Wrap buffer in IIFE    [_k_]  js2 kill      [_lt_] log this
+[_ef_] Extract function        [_iv_] Inline variable    [_ig_] Inject global in IIFE  [_ss_] split string  [_dt_] debug this
+[_ip_] Introduce parameter     [_rv_] Rename variable    [_ee_] Expand node at point   [_sl_] forward slurp
+[_em_] Extract method          [_vt_] Var to this        [_cc_] Contract node at point [_ba_] forward barf
+[_ao_] Arguments to object     [_sv_] Split var decl.    [_uw_] unwrap
+[_tf_] Toggle fun exp and decl [_ag_] Add var to globals
+[_ta_] Toggle fun expr and =>  [_ti_] Ternary to if
+[_q_]  quit"
+      ("ee" js2r-expand-node-at-point)
+      ("cc" js2r-contract-node-at-point)
+      ("ef" js2r-extract-function)
+      ("em" js2r-extract-method)
+      ("tf" js2r-toggle-function-expression-and-declaration)
+      ("ta" js2r-toggle-arrow-function-and-expression)
+      ("ip" js2r-introduce-parameter)
+      ("lp" js2r-localize-parameter)
+      ("wi" js2r-wrap-buffer-in-iife)
+      ("ig" js2r-inject-global-in-iife)
+      ("ag" js2r-add-to-globals-annotation)
+      ("ev" js2r-extract-var)
+      ("iv" js2r-inline-var)
+      ("rv" js2r-rename-var)
+      ("vt" js2r-var-to-this)
+      ("ao" js2r-arguments-to-object)
+      ("ti" js2r-ternary-to-if)
+      ("sv" js2r-split-var-declaration)
+      ("ss" js2r-split-string)
+      ("uw" js2r-unwrap)
+      ("lt" js2r-log-this)
+      ("dt" js2r-debug-this)
+      ("sl" js2r-forward-slurp)
+      ("ba" js2r-forward-barf)
+      ("k" js2r-kill)
+      ("q" nil)
+      )))
 
 ;; use tern for js autocompletion
 (use-package tern
